4b377a4c8ff42c0da875d13a45d8c3263effb94e
40195067 <40195067@HY-R104-12.resource.uc>
2025-12-02 12:19:32 +0000

    First commit

---
+++ src/main/java/com/mycompany/gradingprogram/GradingProgram.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradingprogram;
+
+/**
+ *
+ * @author 40195067
+ */
+public class GradingProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
